87fccaca41c952e87e2b43683dfe93eee8dd03d6
Fernando lepore vm <vm_cercatore@hotmail.com>
2022-11-27 15:42:52 -0300

    revisando condicionales

--- app.js
+++ app.js
@@ -3,46 +3,56 @@
 const minimo = 8;
 const vacio = "";
 
-let nombreCompleto = prompt("por favor ingresa tu nombre")
+/* PROMPT DE NOMBRE */
 
+let nombre = prompt("por favor ingresa tu nombre");
 
-if (nombreCompleto == vacio) {    
-    alert(`tu nombre: ${nombreCompleto} esta vacio`)
-       
-}else if(nombreCompleto.length <= minimo){
-    alert(`tu nombre: ${nombreCompleto} es demasiado breve, ingrese 8 caracteres minimo`)    
-    }
-else{
-    
+if (nombre == vacio) {
+  alert(`tu nombre: "${nombre}" esta vacio`);
+} else if (nombre.length < 8) {
+  alert(
+    `tu nombre: "${nombre}" es demasiado breve, ingrese 8 caracteres minimo`
+  );
 }
 
+/* PROMPT DE USUARIO */
 
-let miNombreDeUsuario = prompt("por favor ingresa tu Nombre de Usuario")
+let usuario = prompt("por favor ingresa tu Nombre de Usuario");
 
-
-if (miNombreDeUsuario == vacio) {    
-    alert(`el nombre de usuario:" ${miNombreDeUsuario}" esta vacio`)
-       
-}else if(miNombreDeUsuario.length <= minimo){
-    alert(`SU USUARIO ERRONEO ES: " ${miNombreDeUsuario}" (demasiado breve, ingrese 8 caracteres)`)    
-    }
-else{
-    
+if (usuario == vacio) {
+  alert(`el nombre de usuario: "${usuario}" esta vacio`);
+} else if (usuario.length < minimo) {
+  alert(
+    `USUARIO ERRONEO: " ${usuario}" (demasiado breve, ingrese 8 caracteres)`
+  );
 }
 
+/* PROMPT DE CONTRASEÑA */
 
-let miPassword = prompt("por favor ingresa una contraseña")
+let pass = prompt("por favor ingresa una contraseña");
 
-if (miPassword.length < minimo) {
-    alert(`"${miPassword} CONTRASEÑA INSEGURA "(ingrese al menos 8 digitos)"`);
-}else if (miPassword.length == vacio){
-    alert(`"${miPassword}" CONTRASEÑA ERRONEA (password vacio)`);
-}else if(miPassword.lastIndexOf(' ') !== -1){
-    alert("por favor ingrese una contraseña sin espacios")
+if (pass.length < minimo) {
+  alert(`"${pass}" CONTRASEÑA INSEGURA (ingrese al menos 8 digitos)`);
+} else if (pass.length == vacio) {
+  alert(`"${pass}" CONTRASEÑA ERRONEA (password vacio)`);
+} else if (pass.lastIndexOf(" ") !== -1) {
+  alert("por favor ingrese una contraseña sin espacios");
 }
 
-let saludoInicial = `hola ${nombreCompleto} bienvenido!!!!`
+let saludo = () => {
+  alert(`hola ${nombre} bienvenido!!!!`);
+};
+saludo();
+/* PROMPT DE EMAIL */
 
-alert(saludoInicial)
+let mail = prompt("ingrese nuevo email");
 
-nombreCompleto = prompt("ingrese nuevo email")
+if (!mail.match(/@/)) {
+    alert("el email no contine arroba");
+}else if (!mail.match(/0-9/)) {
+    alert("el email no contine numeros");
+}else if (!mail.match(/a-z/)) {
+    alert("el email no contine letras minusculas");
+}else if (!mail.match(/A-Z/)) {
+    alert("el email no contine letras Mayusculas");
+}

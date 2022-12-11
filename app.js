a08fce0b81675c93beea450288897bc23e3c39aa
FernandoLepore <juliofernandolepore@gmail.com>
2022-12-11 18:27:23 -0300

    testeando buttons

--- app.js
+++ app.js
@@ -5,80 +5,117 @@ const vacio = "";
 
 /* PROMPT DE NOMBRE */
 
-let nombre = prompt("por favor ingresa tu nombre");
+const interaccion1 = () => {
+  let nombre = prompt("por favor ingresa tu nombre");
 
-while (nombre == vacio) {
-  alert(`el nombre : "${nombre}" esta vacio`);
-  nombre = prompt(`tu nombre: "${nombre}" esta vacio`);
-}
-while (nombre.length < minimo) {
-  alert(`el nombre: "${nombre}" es menor a 8 digitos`);
-  nombre = prompt(`tu nombre: "${nombre}" tiene menos de 8 digitos`);
-}
+  while (nombre == vacio) {
+    alert(`el nombre : "${nombre}" esta vacio`);
+    nombre = prompt(`tu nombre: "${nombre}" esta vacio`);
+  }
+  while (nombre.length < minimo) {
+    alert(`el nombre: "${nombre}" es menor a 8 digitos`);
+    nombre = prompt(`tu nombre: "${nombre}" tiene menos de 8 digitos`);
+  }
 
-/* PROMPT DE USUARIO */
+  /* PROMPT DE USUARIO */
 
-let usuario = prompt("por favor ingresa tu Nombre de Usuario");
+  let usuario = prompt("por favor ingresa tu Nombre de Usuario");
 
-while (usuario.lastIndexOf(" ") !== -1) {
+  while (usuario.lastIndexOf(" ") !== -1) {
     alert("por favor ingrese un usuario sin espacios");
     usuario = prompt("ingrese nuevamente su usuario sin espacios");
   }
-while (usuario == vacio) {
-  alert(`el nombre de usuario: "${usuario}" esta vacio`);
-  usuario = prompt(`ingrese nuevamente nombre de usuario`);
-}
-while (usuario.length < minimo) {
-  alert(
-    `USUARIO ERRONEO: " ${usuario}" (demasiado breve, ingrese 8 caracteres)`
-  );
-  usuario = prompt(`ingrese nuevamente nombre de usuario`);
-}
+  while (usuario == vacio) {
+    alert(`el nombre de usuario: "${usuario}" esta vacio`);
+    usuario = prompt(`ingrese nuevamente nombre de usuario`);
+  }
+  while (usuario.length < minimo) {
+    alert(
+      `USUARIO ERRONEO: " ${usuario}" (demasiado breve, ingrese 8 caracteres)`
+    );
+    usuario = prompt(`ingrese nuevamente nombre de usuario`);
+  }
 
-/* PROMPT DE CONTRASEÑA */
+  /* PROMPT DE CONTRASEÑA */
 
-let pass = prompt("por favor ingresa una contraseña");
+  let pass = prompt("por favor ingresa una contraseña");
 
-while (pass.lastIndexOf(" ") !== -1) {
-  alert("por favor ingrese una contraseña sin espacios");
-  pass = prompt("ingrese nuevamente su contraseña sin espacios");
-}
-while (pass.length < minimo) {
-  alert(`"CONTRASEÑA INSEGURA" (ingrese al menos 8 digitos)`);
-  pass = prompt("ingrese nuevamente su contraseña con 8 digitos o mas");
-}
-while (pass.length == vacio) {
-  alert(`"CONTRASEÑA ERRONEA" (password vacio)`);
-  pass = prompt("ingrese nuevamente su contraseña");
-}
+  while (pass.lastIndexOf(" ") !== -1) {
+    alert("por favor ingrese una contraseña sin espacios");
+    pass = prompt("ingrese nuevamente su contraseña sin espacios");
+  }
+  while (pass.length < minimo) {
+    alert(`"CONTRASEÑA INSEGURA" (ingrese al menos 8 digitos)`);
+    pass = prompt("ingrese nuevamente su contraseña con 8 digitos o mas");
+  }
+  while (pass.length == vacio) {
+    alert(`"CONTRASEÑA ERRONEA" (password vacio)`);
+    pass = prompt("ingrese nuevamente su contraseña");
+  }
 
-let saludo = () => {
-  alert(`hola ${nombre} bienvenido!!!!`);
+  let saludo = () => {
+    alert(`hola ${nombre} bienvenido!!!!`);
+  };
+  saludo();
+  /* PROMPT DE EMAIL */
+
+  let mail = prompt("ingrese su correo electronico");
+
+  while (!mail.match(/@/)) {
+    alert("el email no contine arroba");
+    mail = prompt("ingrese nuevamente su mail");
+  }
+
+  document.querySelector("#nombre").innerHTML = ` tu nombre es: "${nombre}"`;
+  document.querySelector(
+    "#user"
+  ).innerHTML = ` tu nombre de usuario es "${usuario}"`;
+  document.querySelector("#mail").innerHTML = ` tu mail es: "${mail}"`;
 };
-saludo();
-/* PROMPT DE EMAIL */
 
-let mail = prompt("ingrese su correo electronico");
+/* pre entrega 2 - OBJETOS - ARREGLOS - DOM - */
 
-while(!mail.match(/@/)) {
-  alert("el email no contine arroba");
-  mail = prompt("ingrese nuevamente su mail");
-}
+const prodSuper = [
+  {
+    id: 685,
+    producto: "arroz",
+    precio: 200,
+    vencimiento: 20 / 01 / 2023,
+    descripcion: "arroz blanco premium",
+    categoria: "alimento",
+    oferta: true,
+  },
+  {
+    id: 589,
+    producto: "harina blancaflor",
+    precio: 150,
+    vencimiento: 02 / 03 / 2023,
+    descripcion: "harina integral",
+    categoria: "alimento",
+    oferta: true,
+  },
+  {
+    id: 884,
+    producto: "dulce de leche",
+    precio: 300,
+    vencimiento: 06 / 06 / 2023,
+    descripcion: "dulce de leche la serenisima",
+    categoria: "alimentos reposteria",
+    oferta: false,
+  },
+  {
+    id: 106,
+    producto: "oregano alicante",
+    precio: 120,
+    vencimiento: 20 / 01 / 2024,
+    descripcion: "oregano en sobre de 100 gramos",
+    categoria: "alimentos condimentos",
+    oferta: false,
+  },
+];
 
-function modificarTexto(){
-    document.querySelector('#nombre').innerHTML = ` tu nombre es: "${nombre}"`
-    document.querySelector('#user').innerHTML = ` tu nombre de usuario es "${usuario}"`
-    document.querySelector('#mail').innerHTML = ` tu mail es: "${mail}"`
-}
-
-/* pre entrega 2 - OBJETOS - ARREGLOS - DOM - */  
-
-const contenedor = document.querySelector(".contenedor2")
-const tarjeta = document.createElement("div")
-
-tarjeta.className = "tarjeta"
-tarjeta.textContent = "CONTENIDO AGREGADO"
-
-console.log(tarjeta)
-
-contenedor.appendChild(tarjeta)
+const filtradoProducto = () => {
+  for (variable in prodSuper) {
+    console.log(variable);
+  }
+};

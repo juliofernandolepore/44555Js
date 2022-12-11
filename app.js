ccc28f07acdaa5b4a598628c0861abb5250e26ce
FernandoLepore <juliofernandolepore@gmail.com>
2022-12-11 20:30:01 -0300

    aplicando descuentos a los objetos con descuento true

--- app.js
+++ app.js
@@ -112,7 +112,130 @@ const prodSuper = [
     categoria: "alimentos condimentos",
     oferta: false,
   },
+  {
+    id: 95,
+    producto: "papel higienico campanita",
+    precio: 500,
+    vencimiento: 20 / 01 / 2044,
+    descripcion: "rollo 300 metros",
+    categoria: "higiene y limpieza",
+    oferta: false,
+  },
+  {
+    id: 93,
+    producto: "rollo de cocina campanita",
+    precio: 500,
+    vencimiento: 20 / 01 / 2044,
+    descripcion: "rollo 200 metros",
+    categoria: "higiene y limpieza",
+    oferta: false,
+  },
+  {
+    id: 90,
+    producto: "trapo de piso 200 hilos",
+    precio: 370,
+    vencimiento: 20 / 01 / 2044,
+    descripcion: "trapo de piso extra absorvente",
+    categoria: "higiene y limpieza",
+    oferta: false,
+  },
+  {
+    id: 401,
+    producto: "terma 1350 cc ",
+    precio: 200,
+    vencimiento: 20 / 01 / 2024,
+    descripcion: "bebida para diluir",
+    categoria: "alimentos y bebidas",
+    oferta: true,
+  },
+  {
+    id: 600,
+    producto: "coca cola 2.5",
+    precio: 450,
+    vencimiento: 20 / 01 / 2024,
+    descripcion: "coca retornable 2.5",
+    categoria: "alimentos y bebidas",
+    oferta: false,
+  },
+  {
+    id: 705,
+    producto: "azucar chango 1 kg",
+    precio: 410,
+    vencimiento: 20 / 01 / 2024,
+    descripcion: "azucar 1 kg",
+    categoria: "alimentos",
+    oferta: false,
+  },
+  {
+    id: 95,
+    producto: "aceite Cocinero",
+    precio: 350,
+    vencimiento: 20 / 01 / 2025,
+    descripcion: "aceite de girasol 1 litro",
+    categoria: "alimentos",
+    oferta: false,
+  },
+  {
+    id: 703,
+    producto: "leche Larga Vida manfrey",
+    precio: 200,
+    vencimiento: 10 / 01 / 2024,
+    descripcion: "litro de leche entera larga vida",
+    categoria: "alimentos",
+    oferta: true,
+  },
+  {
+    id: 88,
+    producto: "jabon de tocador triple rexona",
+    precio: 300,
+    vencimiento: 20 / 01 / 2044,
+    descripcion: "3 jabones fragancia limon",
+    categoria: "higiene y limpieza",
+    oferta: true,
+  },
+  {
+    id: 87,
+    producto: "dentrifico noc 10 grande",
+    precio: 500,
+    vencimiento: 20 / 01 / 2024,
+    descripcion: "dentrifico grande",
+    categoria: "higiene y limpieza",
+    oferta: false,
+  },
+  {
+    id: 05,
+    producto: "pilas Everready 4 unidades",
+    precio: 800,
+    vencimiento: 20 / 01 / 2034,
+    descripcion: "pilas triple a",
+    categoria: "ferreteria",
+    oferta: false,
+  },
 ];
+
+/*  ordenar los elementos alfabeticamente o por id. con copia segura en otra variablee 
+utilizando los siguientes metodos: spread operator, array.slice , */
+
+//if true aplicar metodo filter en array, mostrando en consola cada una de las ofertas
+
+//hacer un promt con el metodo find para buscar un producto y lo muestre en un alert
+const ofertas = () => {
+  let total = 0;
+  for (let i = 0; i < prodSuper.length; i++) {
+    if (prodSuper[i].oferta === true) {
+      console.log(`producto: ${prodSuper[i].producto}`);
+      console.log(`precio de lista: ${prodSuper[i].precio} pesos`);
+      let operacion = (prodSuper[i].precio * 25) / 100;
+      let descuentoAplicado = prodSuper[i].precio - operacion;
+      console.log(`precio final (descuento 25%): ${descuentoAplicado} pesos`);
+      console.log("*****************************");
+      total += 1;
+    }
+  }
+  console.log(`Productos con descuento: ${total}`);
+};
+
+const ordernar = () => {};
 
 const filtradoProducto = () => {
   for (variable in prodSuper) {

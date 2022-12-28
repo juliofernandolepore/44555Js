95c975231cc3ab4dbb0769b1130defd194a8ca7f
FernandoLepore <juliofernandolepore@gmail.com>
2022-12-28 20:11:54 -0300

    trabajando con storage

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 /* PRE ENTREGA 3 - REFACTORIZANDO */
 
-/* CODIGO DURO */
+/* identificadores manipulables (SOLO ID) */
+
+const inpUser = document.querySelector("#usuario");
+const fLogin = document.querySelector("#login");
+const pass = document.querySelector("#password");
+const contForm = document.querySelector("#contenedor-form");
+const cerrar = document.querySelector("#salir");
+const supermercado = document.querySelector("#supermercado");
+const registrarse = document.querySelector("#altaUsuario");
+
+/* CODIGO DURO (verifica login con base de datos simulacion ) */
 
 const constatar = {
   usuario: "julio",
@@ -11,26 +21,49 @@ const constatar = {
 /* funcion convertidora de JS a JSON - reutilizable */
 
 const upLocSt = (llave, valor) => {
-  localStorage.setItem(llave, JSON.stringify(valor));
+  sessionStorage.setItem(llave, JSON.stringify(valor));
   //sin return
 };
 
 /* function recuperadora con valor parseado (JSON a JS) */
 const recupLS = (llave) => {
-  return JSON.parse(localStorage.getItem(llave));
+  return JSON.parse(sessionStorage.getItem(llave));
 };
 
-/* identificadores manipulables */
-const inpUser = document.querySelector("#usuario");
-const fLogin = document.querySelector("#login");
-const pass = document.querySelector("#password");
-
-/* login */
+/* FORMULARIO LOGIN (SUBMIT) */
 fLogin.onsubmit = (evento) => {
   evento.preventDefault();
   if (inpUser.value === constatar.usuario && pass.value === constatar.pass) {
     upLocSt("login", true);
+    contForm.style.display = "none";
+    cerrar.style.display = "block";
+    supermercado.style.display = "block";
+    document.querySelector("h1").textContent = "BIENVENIDO";
   } else {
-    alert("datos erroneos");
+    inpUser.style.border = "3px solid red";
   }
+};
+
+/* funcion scope general validacion de login (token en storage) */
+
+function estadoLogin(clave) {
+  if (clave !== true) {
+    contForm.style.display = "flex";
+  } else {
+    contForm.style.display = "none";
+    cerrar.style.display = "block";
+    supermercado.style.display = "block";
+    registrarse.style.display = "none";
+  }
+}
+/* aqui es login porque es tal cual la llave esta definida en el local
+storage - no confudir con el id del html */
+estadoLogin(recupLS("login"));
+
+cerrar.onclick = () => {
+  sessionStorage.removeItem("login");
+  cerrar.innerHTML = "tu sesion ha expirado, inicie sesion nuevamente";
+  document.querySelector("h1").textContent = "tu sesion ha caducado";
+  supermercado.style.display = "none";
+  estadoLogin(recupLS("login"));
 };
